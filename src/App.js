22b2af83958386146d1ff03a08fc2b087485ce7d
arunrajbhar123 <arunrajbhar1998@gmai.com>
2022-05-24 20:11:30 +0530

    json server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [value, setValue] = useState([]);
+  const handleTakeQuery = () => {
+    fetch("http://localhost:3008/posts", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        Name: text,
+      }),
+    })
+      .then((r) => r.json())
+      .then((d) => {
+        setValue([...value, d]);
+        setText("");
+      });
+  };
+  const [updatePage, setUpdatePage] = useState(1);
+var url=`http://localhost:3008/posts?_page=${updatePage}&_limit=5`
+// console.log(url)
+  useEffect(() => {
+    fetch(url)
+      .then((r) => r.json())
+      .then((d) => {
+        setValue(d);
+      });
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={text}
+        placeholder="enter query"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={handleTakeQuery}>Save</button>
+      <div>
+        {value.map(({ id, Name }) => (
+          <h1 key={id}> {Name}</h1>
+        ))}
+      </div>
+      <button
+        onClick={() => {
+          if (updatePage > 1) {
+            setUpdatePage(updatePage - 1);
+          }
+        }} disabled={updatePage==1}
+      >
+        Previous Page
+      </button>
+      <button onClick={() => {
+
+        if(updatePage<value.length){
+          setUpdatePage(updatePage + 1);
+          
+        }
+
+       
+        
+        
+        }} disabled={!value.length}>Next Page</button>
+     
     </div>
   );
 }
